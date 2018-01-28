66d68587ed4245819c35e9a617bbb4a5d2673846
owen whitley <owen.whitley@mail.utoronto.ca>
2018-01-28 16:38:37 -0500

    minor changes to smith_waterman current version takes max possibe gap of 1

--- smith_waterman.R
+++ smith_waterman.R
@@ -4,7 +4,8 @@ smith_wat <- function(seq1, #character vector of length 1
                       seq2, # character vector of length 2
                       BLOSUM,
                       gap_penalty = 1.0,
-                      rev_seq2 = TRUE) {
+                      rev_seq2 = TRUE,
+                      gap_limit = 1) {
   if (!is.character(seq1) || (length('seq1') != 1)){
     stop()
   } else if (!is.character(seq2) || (length('seq2') != 1)) {
@@ -68,9 +69,12 @@ smith_wat <- function(seq1, #character vector of length 1
   # in case there are multiple solutions
   solutions <- list()
   
+  gap_count <- 0
   for (p in 1:nrow(end_pts)) {
     i <- end_pts[p,1]
     j <- end_pts[p,2]
+    end_s1 <- i-1
+    end_s2 <- j-1
     out_seq1 <- character()
     out_seq2 <- character()
     cont <- TRUE
@@ -81,7 +85,7 @@ smith_wat <- function(seq1, #character vector of length 1
       base1_ind <- which(base_set == base1)
       base2_ind <- which(base_set == base2)
       # Find H[i-1,j-1] + s(ai,bj)
-      option1 <- H[i-1,j-1] + BLOSUM[base1_ind, base2_ind] # if max, then at i and j, perfect alignment
+      opt1 <- H[i-1,j-1] + BLOSUM[base1_ind, base2_ind] # if max, then at i and j, perfect alignment
       opt2 <- H[i - 1, j] - gap_penalty # if max, then gap in seq2 at this position
       opt3 <- H[i, j - 1] - gap_penalty # if max, then gap in seq1 at this position
       opts <- c(opt1, opt2, opt3, 0)
@@ -90,39 +94,89 @@ smith_wat <- function(seq1, #character vector of length 1
         choice <- choice[1] # if more than one choice satisfies max score, take first option (i.e. direct alignment)
       }
       
-      if (choice == 1) {
-        seq1_append <- rownames(H)[i]
-        seq2_append <- colnames(H)[j]
-        i <- i - 1
+      if (choice == 1) { #if we have a 'match' for given pair
+        if(gap_count > 0) {
+          seq1_append <- c(seq1_append, rownames(H)[i])
+          seq2_append <- c(seq2_append, colnames(H)[j])
+        } else if(gap_count == 0) {
+          seq1_append <- rownames(H)[i]
+          seq2_append <- colnames(H)[j]
+        } else {
+          stop('this should not be possible')
+        }
+        gap_count <- 0
+        i <- i - 1 #back increment i and j
         j <- j - 1
-      } else if (choice == 2) {
-        seq1_append <- rownames(H)[i]
-        seq2_append <- '-'
+      } else if (choice == 2) { # else if we have a gap in seq2 (columns)
+        if(gap_count > 0) {
+          seq1_append <- c(seq1_append, rownames(H)[i])
+          seq2_append <- c(seq2_append, '-')
+        } else if(gap_count == 0) {
+          seq1_append <- rownames(H)[i]
+          seq2_append <- '-'
+        } else {
+          stop('this should not be possible')
+        }
         i <- i - 1
-      } else if (choice == 3) {
-        seq1_append <- '-'
-        seq2_append <- colnames(H)[j]
+        gap_count <- gap_count + 1
+      } else if (choice == 3) { # we have a gap in sequence 1
+        if(gap_count > 0) {
+          seq1_append <- c(seq1_append, '-')
+          seq2_append <- c(seq2_append, colnames(H)[j])
+        } else if(gap_count == 0) {
+          seq1_append <- '-'
+          seq2_append <- colnames(H)[j]
+        } else {
+          stop('this should not be possible')
+        }
         j <- j -1
+        gap_count <- gap_count + 1
       } else {
         stop('for some reason choice not made')
       }
+      # Terminate if the maximum of options is less than or equal to 0, or if we've hi i = 0 or j = 0
       if (max(opts) <= 0) {
         cont <- FALSE
       } else if ((i == 0) || (j == 0)) {
         cont <- FALSE
+      } else if (H[i,j] == 0) {
+        break
       }
-      out_seq1 <- c(out_seq1, seq1_append)
-      out_seq2 <- c(out_seq2, seq2_append)
-      # Terminate if the maximum of options is less than or equal to 0, or if we've hi i = 0 or j = 0
+      if (gap_count == 0){
+        out_seq1 <- c(out_seq1, seq1_append)
+        out_seq2 <- c(out_seq2, seq2_append)
+      } else if (gap_count > gap_limit) {
+        break
+      }
+      
+      
 
     } # end while loop
-    ali_seq <- rep('|', length(out_seq1))
-    ali_seq[out_seq1 == '-'] <- ' ' 
-    ali_seq[out_seq2 == '-'] <- ' '
-    ali_seq <- paste(rev(ali_seq), collapse = '')
-    out_seq1 <- paste(rev(out_seq1), collapse = '')
-    out_seq2 <- paste(rev(out_seq2), collapse = '')
-    solutions[[p]] <- list( seq1_ali = out_seq1, alignment = ali_seq, seq2_ali = out_seq2, score = max(H))
+    if ((length(out_seq1) > 0) && (length(out_seq2) > 0)) {
+      ali_seq <- rep('|', length(out_seq1))
+      ali_seq[out_seq1 == '-'] <- ' ' 
+      ali_seq[out_seq2 == '-'] <- ' '
+      ali_seq <- paste(rev(ali_seq), collapse = '')
+      ali_seq <- paste('  ', ali_seq, '  ')
+      start_s1 <- i-1
+      out_seq1 <- paste(rev(out_seq1), collapse = '')
+      out_seq2 <- paste(rev(out_seq2), collapse = '')
+      out_seq1 <- paste('5\'', out_seq1, '3\'')
+      if(rev_seq2) {
+        start_s2 <- length(seq2) - end_s2 + 1
+        end_s2 <- length(seq2) - (j- 1) + 1 
+        out_seq2 <- paste('3\'', out_seq2, '5\'')
+      } else {
+        out_seq2 <- paste('5\'', out_seq2, '3\'')
+      }
+      solutions[[p]] <- list( seq1_ali = out_seq1, alignment = ali_seq, seq2_ali = out_seq2, score = max(H), location = end_pts[p,],
+                              seq1_start_end = c(start_s1, end_s1), seq2_start_end = c(start_s2, end_s2))
+    } else {
+      solutions[[p]] <- list(score = max(H), location = end_pts[p,])
+    }
+    
+    
+    
   } # end larger for loop
   return(solutions)
 }
@@ -135,5 +189,9 @@ print_solutions <- function(solutions) {
     print(paste('seq1_ali', solution_i$seq1_ali))
     print(paste('align   ', solution_i$alignment))
     print(paste('seq2_ali', solution_i$seq2_ali))
+    print('seq1 position')
+    print(solution_i$seq1_start_end)
+    print('seq2 position')
+    print(solution_i$seq2_start_end)
   }
 }
